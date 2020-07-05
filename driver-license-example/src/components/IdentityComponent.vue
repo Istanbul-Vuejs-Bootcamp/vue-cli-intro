38a1e96765f339b2d221c4d60614c96fe79000cb
Mustafa Demir <mustafa.demir@kboxglobal.com>
2020-07-05 12:10:02 +0300

    component example

---
+++ driver-license-example/src/components/IdentityComponent.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="card bg-secondary">
+        <div class="row">
+            <div class="col-3 pt-1">
+                <img :src="driverLicenseObject.photo" width="100" />
+            </div>
+            <div class="col-9 text-left text-white">
+                <div>{{ name }} {{ surname }}</div>
+                <div>{{ driverLicenseObject.licenseId }}</div>
+                <div>{{ driverLicenseObject.licenseType }}</div>
+                <div>{{ driverLicenseObject.publishDate }}</div>
+                <div>{{ driverLicenseObject.expireDate }}</div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "IdentityComponent",
+        props: {
+            name: {
+                type: String,
+                required: true,
+            },
+            surname: {
+                type: String,
+                required: true,
+            },
+            driverLicenseObject: {
+                type: Object,
+                required: true,
+            }
+        }
+
+    }
+</script>
