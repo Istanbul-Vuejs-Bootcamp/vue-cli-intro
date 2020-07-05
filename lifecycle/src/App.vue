26f31ec0fbd376221d4fbdb4e7432cf6eb74c4b3
Mustafa Demir <mustafa.demir@kboxglobal.com>
2020-07-05 14:12:38 +0300

    example

---
+++ lifecycle/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <button @click="isShow = !isShow">Göster / Gizle</button>
+        <firstComponent v-if="isShow"/>
+        <hr>
+
+        <!-- template tag'i dom'a asla render edilmez -->
+        <template v-if="isShow">
+            <firstComponent />
+        </template>
+        <!-- div tag'i dom'a render edilecek (isShow = true ise) -->
+        <div v-if="isShow">
+            <firstComponent />
+        </div>
+
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'App',
+        components: {
+            firstComponent: () => import('./components/firstComponent')
+        },
+        data() {
+            return {
+                isShow: false,
+            }
+        },
+    }
+</script>
---
+++ lifecycle/src/components/firstComponent.vue
@@ -0,0 +1,54 @@
+<template>
+ <div v-if="isRender">
+
+  <div>Hello. I am first component</div>
+
+  <div>{{ message }}</div>
+
+  <button @click="message = 'hello'">Mesajı Değiştir</button>
+ </div>
+</template>
+
+<script>
+    export default {
+       name: "firstComponent",
+       data() {
+        return {
+         message: '',
+         isRender: false,
+        }
+       },
+
+       beforeCreate() {
+        console.log('beforeCreate is here');
+       },
+       created() {
+        setTimeout(()=> {
+         this.isRender = true;
+        }, 2000)
+        console.log('created is here');
+       },
+       beforeMount() {
+        console.log('beforeMount is here');
+       },
+       mounted() {
+        console.log('mounted is here');
+       },
+       beforeUpdate() {
+        console.log('beforeUpdate is here');
+       },
+       updated() {
+         console.log('udpated is here');
+        },
+       beforeDestroy() {
+        console.log('beforeDestroy is here');
+       },
+       destroyed() {
+        console.log('destroyed is here');
+       }
+    }
+</script>
+
+<style scoped>
+
+</style>
